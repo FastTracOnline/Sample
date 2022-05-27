eab98a670dca139e60c5d234eaa8acc51a56abcf
foxphile txwhitehouse.net <foxphile@txwhitehouse.net>
2022-05-27 15:43:36 -0500

    Initial Upload

---
+++ Football/Program.cs
@@ -0,0 +1,92 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Football
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{DateTime.Now} Football Application Started");
+
+            var digitsOnly = "0123456789";
+            var pathName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "";
+            var fileName = "football.dat";
+            var fullFileName = Path.Combine(pathName, fileName);
+            List<TeamGoals> teamList = new List<TeamGoals>();
+
+            // 1. Check if file exists
+            // 2. Read text file into an array of strings
+            // 3. Iterate array and parse into List of objects
+            // 4. Locate and display target value
+
+            // Data is defined into specific columns.
+            // Every column of every record has data, so using Split to parse works fine.
+            // Each column must be "cleansed" and tested for type.
+            // First column is list #.  If not numeric, reject the entire line.
+            // Second column is Team Name (allow trimmed strings)
+            // Seventh column is Goals Scored FOR the team vs opponents
+            // Eighth column is Goals Scored AGAINST the team by opponents
+
+            if (File.Exists(fullFileName))
+            {
+                var lines = File.ReadAllLines(fullFileName);
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    var lineToCheck = lines[i];
+                    if (string.IsNullOrEmpty(lineToCheck) || lineToCheck.Length < 20)
+                        continue;
+
+                    var fields = lineToCheck.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    if (fields.Length < 9)
+                        continue;
+
+                    var newTeamNumber = fields[0].OnlyValidCharacters(digitsOnly);
+                    var newTeamName = fields[1].Trim();
+                    var newGoalsFor = fields[6].OnlyValidCharacters(digitsOnly);
+                    var newGoalsAgainst = fields[8].OnlyValidCharacters(digitsOnly);
+
+                    if (string.IsNullOrEmpty(newTeamNumber))
+                        continue;       // Non-numeric characters in Team # - Reject
+
+                    teamList.Add(new TeamGoals()
+                    {
+                        TeamName = newTeamName,
+                        GoalsFor = int.Parse(newGoalsFor),
+                        GoalsAgainst = int.Parse(newGoalsAgainst)
+                    });
+                }
+
+                // It was not specified that Goals For must be > Goals Against, so ABS() was used
+                var result = teamList.OrderBy(x => x.GoalSpread).ToList().First();
+                Console.WriteLine($"{DateTime.Now} Team with smallest Goal Spread was {result.TeamName}");
+            }
+            Console.WriteLine($"{DateTime.Now} Football Application Ended");
+        }
+    }
+
+    internal class TeamGoals
+    {
+        public string? TeamName { get; set; }
+        public int GoalsFor { get; set; }
+        public int GoalsAgainst { get; set; }
+        public int GoalSpread => Math.Abs(GoalsFor - GoalsAgainst);
+    }
+
+    public static class Extensions
+    {
+        public static string OnlyValidCharacters(this string inputString, string validCharacters)
+        {
+            if (string.IsNullOrEmpty(inputString))
+                return inputString;
+
+            var invalidCharacters = Regex.Replace(inputString, $"[{validCharacters}]", "");
+
+            if (string.IsNullOrEmpty(invalidCharacters))
+                return inputString;
+            else
+                return Regex.Replace(inputString, $"[{invalidCharacters}]", "");
+        }
+    }
+}
---
+++ Weather/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace Weather
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{DateTime.Now} Weather Application Started");
+
+            var digitsOnly = "0123456789";
+            var pathName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? "";
+            var fileName = "weather.dat";
+            var fullFileName = Path.Combine(pathName, fileName);
+            List<DailyWeather> weatherList = new List<DailyWeather>();
+
+            // 1. Check if file exists
+            // 2. Read text file into an array of strings
+            // 3. Iterate array and parse into List of objects
+            // 4. Locate and display target value
+
+            // Data is defined into specific columns.
+            // Not every column of every record has data (sparse), so using Substring to parse works fine.
+            // Each column must be "cleansed" and tested for type.
+            // First column is day #.  If not numeric, reject the entire line.
+            // Second column is Max Temperature on that day
+            // Third column is Min Temperature on that day
+
+            if (File.Exists(fullFileName))
+            {
+                var lines = File.ReadAllLines(fullFileName);
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    var lineToCheck = lines[i];
+                    if (string.IsNullOrEmpty(lineToCheck) || lineToCheck.Length < 20)
+                        continue;
+
+                    var newDayofMonth = lineToCheck.Substring(0, 4).OnlyValidCharacters(digitsOnly);
+                    var newMaxTemperature = lineToCheck.Substring(5, 6).OnlyValidCharacters(digitsOnly);
+                    var newMinTemperature = lineToCheck.Substring(12, 6).OnlyValidCharacters(digitsOnly);
+
+                    if (string.IsNullOrEmpty(newDayofMonth))
+                        continue;       // Non-numeric characters in Day of Month - Reject
+
+                    weatherList.Add(new DailyWeather()
+                    {
+                        DayofMonth = int.Parse(newDayofMonth),
+                        MaxTemperature = int.Parse(newMaxTemperature),
+                        MinTemperature = int.Parse(newMinTemperature)
+                    });
+                }
+
+                var result = weatherList.OrderBy(x => x.TemperatureSpread).ToList().First();
+                Console.WriteLine($"{DateTime.Now} Day with smallest Temperature Spread was {result.DayofMonth}");
+            }
+            Console.WriteLine($"{DateTime.Now} Weather Application Ended");
+        }
+    }
+
+    internal class DailyWeather
+    {
+        public int DayofMonth { get; set; }
+        public int MaxTemperature { get; set; }
+        public int MinTemperature { get; set; }
+        public int TemperatureSpread => MaxTemperature - MinTemperature;
+    }
+
+    public static class Extensions
+    {
+        public static string OnlyValidCharacters(this string inputString, string validCharacters)
+        {
+            if (string.IsNullOrEmpty(inputString))
+                return inputString;
+
+            var invalidCharacters = Regex.Replace(inputString, $"[{validCharacters}]", "");
+
+            return Regex.Replace(inputString, $"[{ invalidCharacters}]", "");
+        }
+    }
+}
